e7a0c3d300532783a9544e62824323e3e2203343
klivesSTACK <thisismebriones@gmail.com>
2022-07-05 15:54:51 +0800

    Joke View Layer Created. closes #5

--- src/main/java/guro/springframework/spring5jokesappv2/controllers/JokeController.java
+++ src/main/java/guro/springframework/spring5jokesappv2/controllers/JokeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokeController {
+
     private final JokeService jokeService;
 
     public JokeController(JokeService jokeService) {
